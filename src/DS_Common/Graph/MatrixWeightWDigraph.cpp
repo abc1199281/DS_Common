05b83bcbd1ce83ac83afc49199b12e878d7e6bb3
powei <abc1199281@gmail.com>
2021-10-09 23:32:10 +0800

    [Dev] basic load graph data.

--- src/DS_Common/Graph/MatrixWeightWDigraph.cpp
+++ src/DS_Common/Graph/MatrixWeightWDigraph.cpp
@@ -55,14 +55,28 @@ namespace DS_Common {
 		}
 			
 		int a, b, cost;
+		int directional, weighted;
+		infile >> weighted >> directional;
+		std::cout << "Weighted: " << weighted << "& directional:" << directional << std::endl;
 		infile >> this->numVer >> this->numEdge;
 		std::cout << "V: " << numVer << "& Edge:" << numEdge << std::endl;
-		this->adj = std::vector<std::vector<int>>(numVer, std::vector<int>(numVer, 0));
+		this->adj = std::vector<std::vector<double>>(numVer, std::vector<double>(numVer, 0));
 		for (int i = 0; i < numEdge; i++)
 		{
-			infile >> a >> b >> cost;
-			this->adj[a][b] = cost;
-			std::cout << "Add V: " << a << "& " << b << ", cost" << cost << std::endl;
+			if (weighted == 1)
+			{
+				infile >> a >> b >> cost;
+				this->adj[a][b] = cost;
+				if(directional==0)
+					this->adj[b][a] = cost;
+				std::cout << "Add V: " << a << "& " << b << ", cost" << cost << std::endl;
+			}else {
+				infile >> a >> b;
+				this->adj[a][b] = 1;
+				if (directional == 0)
+					this->adj[b][a] = 1;
+				std::cout << "Add V: " << a << "& " << b << std::endl;
+			}
 		}
 		infile.close();
 	}
